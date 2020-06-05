86ddfffebd78c75190cdf54ca7c61e20acfd973c
Lucas Santos Lima <lucas.galrao@hotmail.com>
2020-06-04 21:49:00 -0300

    Nova build test

--- pandoro2/src/config.ts
+++
@@ -1,6 +0,0 @@
-const config = {
-  store_id: 30,
-  baseURL: "https://f98aeb4fd01c.ngrok.io"
-}
-
-export default config;
---
+++ tests/src/config.ts
@@ -0,0 +1,6 @@
+const config = {
+  store_id: 9999,
+  baseURL: "https://api-saef-delivery.herokuapp.com"
+}
+
+export default config;
